fcc62528d7c73b8bb24cec5153fa548199327248
wall-e785 <wall_echau604@yahoo.ca>
2025-01-14 21:56:37 -0800

    Added Header

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!--HOME PAGE FOR RECIPE SITE-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recipes</title>
+</head>
 
+<body>
+    <header>
+        <h1>Wallace's Recipes</h1>
+        <nav>
+            <a href="index.php">All Recipes</a>
+            <a href="add-recipe.php">Add a New Recipe</a>
+        </nav>
+    </header>
+    
+</body>
+</html>
