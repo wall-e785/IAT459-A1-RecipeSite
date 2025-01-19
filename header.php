59647c9a1e3359d1b941b6ec074bf7fd9df3a22c
wall-e785 <wall_echau604@yahoo.ca>
2025-01-19 00:02:56 -0800

    Added styling for header

---
+++ header.php
@@ -0,0 +1,22 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recipes</title>
+    <style>
+        a { color:black;}
+    </style>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <header>
+    <table style="width:100%;border-spacing:0px">
+    <tr style="height:100px;background-color:545852">
+    <th style="font-family:verdana;font-size:66px;color:white;text-align:center;background-color:green">Wallace's Recipes</th>
+    </tr>
+    <tr height="30" bgcolor="#858585">
+    <td style="font-family:verdana;font-size:24px;color:white;text-align:center">
+    <a href="index.php" class="main-nav">All Recipes</a> |
+    <a href="add-recipe.php" class="main-nav">Add a New Recipe</a>
+    </td>
+    </header>
--- index.php
+++ index.php
@@ -1,21 +1,8 @@
 <!--HOME PAGE FOR RECIPE SITE-->
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recipes</title>
-</head>
-
-<body>
-    <header>
-        <h1>Wallace's Recipes</h1>
-        <nav>
-            <a href="index.php">All Recipes</a>
-            <a href="add-recipe.php">Add a New Recipe</a>
-        </nav>
-    </header>
     <?php
+        require 'header.php';
         $document_root = $_SERVER['DOCUMENT_ROOT'];
         print $document_root;
         $recipes = @fopen("$document_root/../recipes/recipes.txt", 'r');
@@ -24,9 +11,14 @@
         }else{
             //keep track of which line in the file you're on
             $line_num = 0;
+            echo "<table id=\"recipe-table\">";
             while($arr = fgetcsv($recipes)){
                 $line_num++;
-                echo "<table><tr>";
+                if($line_num%2 == 0){
+                    echo "<tr class=\"dark-row\">";
+                }else{
+                    echo "<tr class=\"light-row\">";
+                }
                 // for($i=0; $i<sizeof($arr); $i++){
                 //     echo "<td><p>$arr[$i]</p><td>";
                 // }
@@ -34,9 +26,14 @@
                 //in order to be able to use $_GET on the details.php page, I need to add the line number to the URL
                 //referenced from: https://stackoverflow.com/questions/47304899/php-a-href-how-to-pass-value-to-another-page-when-click-the-href-and-how-to-r#:~:text=You%20simply%20add%20your%20data,by%20using%20the%20GET%20Method.&text=%24_GET%5B'id'%5D%3B
                 $link = "details.php?recipe_id=" . $line_num;
-                echo "<td><a href=" . $link .  ">" . $arr[0] . "</a><td>";
-                echo "</tr></table>";
+                if($line_num%2 == 0){
+                    echo "<td><a href=" . $link .  " class=\"recipe-nav\">" . $arr[0] . "</a><td>";
+                }else{
+                    echo "<td><a href=" . $link .  " class=\"recipe-nav\">" . $arr[0] . "</a><td>";
+                }
+                echo "</tr>";
             }
+            echo "</table>";
         }
 
         fclose($recipes);
